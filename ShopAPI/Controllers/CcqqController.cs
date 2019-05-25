5d4867f0c2264ad295635ab82e3a625174269a71
lpnam <lenam9412@gmail.com>
2019-05-25 14:10:26 +0700

    cm

---
+++ ShopAPI/Controllers/CcqqController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopAPI.Controllers
+{
+    public class CcqqController : Controller
+    {
+        public IActionResult Index()
+        {
+            // dm test2
+            return View();
+        }
+    }
+}
